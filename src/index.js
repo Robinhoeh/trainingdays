df86ae0b5081e0f31d5c3e7b769ca2860bb851bc
Robin Watson <drumsrobin@gmail.com>
2017-10-17 10:51:33 -0400

    inital javascript to change scope on

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
-import './index.css';
+let allEvents;
+let event;
+const eventTrainingTimes = {'Marathon': 50, 'Triathlon': 100, 'Decathlon': 200 };
 
-document.getElementById('root').innerHTML = 'Hello World';
+/* this is used a few places and it is vulnerable */ 
 
-alert('Hello World!');
+const getRandomEvent = () => {
+  allEvents = ['Marathon', 'Triathlon', 'Decathlon'];
+  event = allEvents[Math.floor(Math.random() * allEvents.length)];
+};
+
+
+const getEventActivities = () => {
+  allEvents = ['Marathon', 'Triathlon', 'Decathlon'];
+
+  if (!allEvents.includes(event)) {
+    return null; 
+  }
+  
+  if (event === 'Marathon') {
+    const activities = ['Running'];
+    return activities.join(', '); 
+  } 
+  if (event === 'Triathlon') {
+    const activities = ['Running', 'Cycling', 'Swimming'];
+    return activities.join(', '); 
+  } 
+  if (event === 'Decathlon') {
+    const activities = ['Running', 'Hurdles', 'Javelin throw', 'Discus Throw', 'Shot put', 'High Jump'];
+    return activities.join(', '); 
+  }
+  
+};
+
+const getDaysToTrain = () => {
+  allEvents = ['Marathon', 'Triathlon', 'Decathlon'];
+  if (!allEvents.includes(event)) {
+    return null; 
+  }
+  
+  return eventTrainingTimes[event];
+};
+
+
+getRandomEvent();
+console.log('Your event is a ' + event + '. Your event activities consist of ' + getEventActivities() + '. You have ' + getDaysToTrain() +  ' days to train.');
+
